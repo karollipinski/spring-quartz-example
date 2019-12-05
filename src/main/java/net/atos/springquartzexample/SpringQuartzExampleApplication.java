18aa165b6dd00ba340b6af7e5f770f9d21802528
klipinski <karol.lipinski@fenige.pl>
2019-12-05 08:07:15 +0100

    Create quartz example

---
+++ src/main/java/net/atos/springquartzexample/SpringQuartzExampleApplication.java
@@ -0,0 +1,13 @@
+package net.atos.springquartzexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuartzExampleApplication.class, args);
+	}
+
+}
